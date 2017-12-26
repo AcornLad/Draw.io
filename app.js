bcc026ad507db733ca140dd7552210baee334e69
Conrad Cash (Lake Mary) <conrad.cash@superion.com>
2017-12-26 14:34:54 -0500

    Added client-side content from code and coffee

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//All lines ever drawn
+var line_history = [];
+
+io.on('connection', function(socket){
+  //First send history to client
+  for (var i in line_history){
+    socket.emit('draw_line', { line: line_history[i] });
+  }
+  //Then handle message type "draw_line"
+  socket.on('draw_line', function(data){
+    line_history.push(data.line);
+    io.emit('draw_line', { line: data.line });
+  });
+});
+
 
 
 http.listen(3000, function(){
